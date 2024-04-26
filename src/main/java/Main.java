1e6f94de8aca5b0fc5ad648f7c17e7735f2ff325
jinha <gammazx@naver.com>
2024-04-26 14:05:58 +0900

    feature : getGop, getZegop 기능 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-    		// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
-    		System.out.println("HI");
-			System.out.println("HOHO");
-    	}
+        System.out.println("HI");
+
+		Cal cal = new Cal();
+
+		//Test
+		System.out.println("getGop(3,5) : " + cal.getGop(3,5));
+		System.out.println("getZegop(3) : " + cal.getZegop(3));
+    }
+}
+
+class Cal {
+	int getGop(int a, int b) {
+		return a * b;
+	}
+
+	int getZegop(int a) {
+		return a * a;
+	}
 }
