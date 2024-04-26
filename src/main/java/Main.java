52effb87ca57d2ff60e1560b894c39af94144111
chlduq04 <chlduq04@gmail.com>
2024-04-26 14:05:00 +0900

    getMinus 무지성 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-    		// TODO Auto-generated method stub
+    public static int getMinus(final int a, final int b) {
+        return a - b;
+    }
 
-    		System.out.println("HI");
-    	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("HI");
+    }
 }
