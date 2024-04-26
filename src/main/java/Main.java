211193680e4086191ca5a0776de2fd739e03ecb4
dowon-ahn <ahndw55@gmail.com>
2024-04-26 14:04:46 +0900

    Main 클래스 내 메서드 추가 - getDivie(int a, int b) - getSumSum(int a, int b, int c)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
         System.out.println("HI");
-        System.out.println("기능추가");
+    }
+
+    public int getDivie(int a, int b){
+        if(b==0) throw new ArithmeticException("Divie by zero");
+        return a/b;
+    }
+
+    public int getSumSum(int a, int b, int c){
+        return a + b + c;
     }
 }
