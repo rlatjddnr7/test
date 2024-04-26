ff4717d866ac408a140a96fd1b32e7f6e793ea83
dowon-ahn <ahndw55@gmail.com>
2024-04-26 13:52:55 +0900

    기능추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    		// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
-    		System.out.println("HI");
-    	}
+        System.out.println("HI");
+        System.out.println("기능추가");
+    }
 }
